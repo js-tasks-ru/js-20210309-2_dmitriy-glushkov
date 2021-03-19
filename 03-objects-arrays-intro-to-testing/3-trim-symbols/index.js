abb36890ac6e9af09bc6e14e060b35cbda7f6057
dglushko <dmitrii.glushkov@t-systems.com>
2021-03-20 00:28:00 +0300

    solved 3d part

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -6,4 +6,29 @@
  */
 export function trimSymbols(string, size) {
 
+  if (string === '' || size === 0) {
+    return '';
+  }
+  if (size === undefined) {
+    return string;
+  }
+
+  let resString = '';
+  let tmpArr = [];
+
+  for (let charPos = 0; charPos < string.length; charPos++) {
+
+    tmpArr.push(string[charPos]);
+
+    if (string[charPos] !== string[charPos + 1]) {
+
+      const charsToAdd = (size < tmpArr.length) ? size : tmpArr.length;
+
+      for (let char = 0; char < charsToAdd; char++) {
+        resString = resString + tmpArr[char];
+      }
+      tmpArr = [];
+    }
+  }
+  return resString;
 }
