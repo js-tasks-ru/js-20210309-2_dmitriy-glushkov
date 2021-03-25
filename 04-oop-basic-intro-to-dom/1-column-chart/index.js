a4f9a82d1c30e162348081bd5e1c7cce962c73c9
dglushko <dmitrii.glushkov@t-systems.com>
2021-03-25 14:38:19 +0300

    Part 4 fix all remarks

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -14,23 +14,30 @@ export default class ColumnChart {
   }
 
 
+  setColumnChartType () {
+    return (!this.data || this.data.length === 0) ?
+      `<div class="column-chart column-chart_loading" style="--chart-height: 50">` :
+      `<div class="column-chart" style="--chart-height: 50">`;
+  }
+
+
   pasteColumns() {
     let res = "";
-    const maxValue = Math.max(...this.data);
+    if (!this.data || this.data.length === 0) {
+      res = `<div ><img src="charts-skeleton.svg" alt="zaglushka"></div>`;
+    }
+    else {
+      res += `<div data-element="body" class="column-chart__chart">`;
 
-    for (let value of this.data) {
+      const maxValue = Math.max(...this.data);
 
-      if (value === maxValue) {
-        res = res + `<div style="--value: ${this.chartHeight}" data-tooltip="100%"></div>`;
-      }
-      else {
+      [...this.data].map(value => {
 
         let columnHeight = Math.floor(value * this.chartHeight / maxValue);
         let tooltipValue = Math.round(value / maxValue * 100);
 
-        res = res + `<div style="--value: ${columnHeight}" data-tooltip="${tooltipValue}%"></div>`;
-      }
-
+        res += `<div style="--value: ${columnHeight}" data-tooltip="${tooltipValue}%"></div>`;
+      });
     }
     return res;
   }
@@ -38,49 +45,37 @@ export default class ColumnChart {
 
   render() {
     const element = document.createElement('div'); // (*)
-
-    if (!this.data || this.data.length === 0) {
-      element.innerHTML = `
-        <div class="column-chart column-chart_loading" style="--chart-height: 50">
-          <div class="column-chart__title">
-            Total ${this.label}
-            <a class="column-chart__link" href="${this.link}">View all</a>
-          </div>
-          <div data-element="header" class="column-chart__header">${this.value}</div>
-          <div >
-              <img src="charts-skeleton.svg" alt="zaglushka">
-          </div>
-        </div>
-      `;
-    }
-    else {
-      element.innerHTML = `
-        <div class="column-chart" style="--chart-height: 50">
-          <div class="column-chart__title">
-            Total ${this.label}
-            <a class="column-chart__link" href="${this.link}">View all</a>
-          </div>
-          <div class="column-chart__container">
-            <div data-element="header" class="column-chart__header">${this.value}</div>
-            <div data-element="body" class="column-chart__chart">
-               ${this.pasteColumns()}
-            </div>
-          </div>
-        </div>
-    `;}
+    element.innerHTML = `
+      ${this.setColumnChartType()}
+      <div class="column-chart__title">
+        Total ${this.label}
+        <a class="column-chart__link" href="${this.link}">View all</a>
+      </div>
+      <div data-element="header" class="column-chart__header">${this.value}</div>
+         ${this.pasteColumns()}
+      </div>
+    `;
 
     // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
     // который мы создали на строке (*)
     this.element = element.firstElementChild;
   }
 
-  update() {
 
+  update(newData) {
+
+    const newDataElement = document.createElement('div');
+    newDataElement.innerHTML = this.pasteColumns(newData);
+
+    const chart = this.element.querySelector('.column-chart__chart');
+    chart.append(newDataElement);
   }
+
 
   remove () {
     this.element.remove();
   }
+
 
   destroy() {
     this.remove();
