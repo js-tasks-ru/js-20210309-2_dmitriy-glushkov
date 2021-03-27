f8098b253650830f947d453ea0607139ae1d7db2
dglushko <dmitrii.glushkov@t-systems.com>
2021-03-28 01:26:56 +0300

    1 task solved with 1 test constantly failing

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
 
+  static notificationInstances = []
+
+  constructor(message = '', {duration = 0, type = 'success'} = {}) {
+
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.show();
+
+    NotificationMessage.notificationInstances = [];
+    NotificationMessage.notificationInstances.push(this);
+
+    setTimeout(() => this.destroy(), this.duration);
+  }
+
+
+  show (whatsTheHell) {
+
+    if (NotificationMessage.notificationInstances.length !== 0) {
+      for (let NI of NotificationMessage.notificationInstances) {
+        NI.remove();
+      }
+    }
+
+    const element = document.createElement('div');
+    element.className = this.type;
+    element.innerHTML = `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">${this.message}</div>
+        </div>
+      </div>
+    `;
+
+    this.element = element;
+
+    document.body.append(this.element);
+
+    if (whatsTheHell) {
+      whatsTheHell.append(this.element);
+    }
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
