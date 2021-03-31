0b27a6ae7852a1d9cafc170349d448f87be2e9ba
dglushko <dmitrii.glushkov@t-systems.com>
2021-04-01 00:56:29 +0300

    Correct behavior with event variable

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -8,7 +8,7 @@ class Tooltip {
   }
 
 
-  render() {
+  render(event) {
 
     if (Tooltip.linkToInstance !== undefined) {
       Tooltip.linkToInstance.remove();
@@ -29,11 +29,17 @@ class Tooltip {
     this.element = element;
 
     const targetElement = document.querySelector('[data-tooltip]');
-    console.log('targetElement', targetElement)
-    console.log('targetElement.getBoundingClientRect()', targetElement.getBoundingClientRect())
+    //console.log('targetElement', targetElement)
+    //console.log('targetElement.getBoundingClientRect()', targetElement.getBoundingClientRect())
 
-    element.style.left = targetElement.getBoundingClientRect().left + 5 + 'px';
-    element.style.top = targetElement.getBoundingClientRect().bottom + 5 + 'px';
+    console.log('event.clientX', event.clientX)
+    console.log('event.clientY', event.clientY)
+
+    element.style.left = event.clientX + 5 + 'px';
+    element.style.top = event.clientY + 'px';
+
+    // element.style.left = targetElement.getBoundingClientRect().left + 5 + 'px';
+    // element.style.top = targetElement.getBoundingClientRect().bottom + 5 + 'px';
 
 
     document.body.append(this.element);
@@ -50,9 +56,14 @@ class Tooltip {
     //const toolTipText = document.querySelector('[data-tooltip]') //data-tooltip="bar-bar-bar"
     const targetElement = document.querySelector('[data-tooltip]');
 
-    targetElement.addEventListener('pointerover', () => this.render());
+    //targetElement.addEventListener('pointerover', () => this.render('pointerover'));
 
+    targetElement.addEventListener('pointerover', () => this.render(event));
+
+
+    targetElement.addEventListener('pointerout', () => this.destroy());
     //document.addEventListener('pointerover', () => this.render());
+
 
   }
 
