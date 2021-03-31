9f9e06e632bfb6cb5673d0a4b38ebe0bf7542608
dglushko <dmitrii.glushkov@t-systems.com>
2021-04-01 00:08:25 +0300

    Tooltip all tests green

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,75 @@
 class Tooltip {
 
+  static linkToInstance;
+
+  constructor() {
+
+
+  }
+
+
+  render() {
+
+    if (Tooltip.linkToInstance !== undefined) {
+      Tooltip.linkToInstance.remove();
+    }
+
+    const toolTipText = document.querySelector('[data-tooltip]').getAttribute('data-tooltip') //data-tooltip="bar-bar-bar"
+    //console.log('toolTipText', toolTipText)
+    //console.log('toolTipText', toolTipText.getAttribute('data-tooltip'))
+
+    const element = document.createElement('div');
+    //element.className = this.type;
+    element.className = "tooltip";
+    element.innerHTML = `
+      <div>
+        ${toolTipText}
+      </div>
+    `;
+    this.element = element;
+
+    const targetElement = document.querySelector('[data-tooltip]');
+    console.log('targetElement', targetElement)
+    console.log('targetElement.getBoundingClientRect()', targetElement.getBoundingClientRect())
+
+    element.style.left = targetElement.getBoundingClientRect().left + 5 + 'px';
+    element.style.top = targetElement.getBoundingClientRect().bottom + 5 + 'px';
+
+
+    document.body.append(this.element);
+
+
+
+    Tooltip.linkToInstance = this;
+
+
+  }
+
+  initialize() {
+
+    //const toolTipText = document.querySelector('[data-tooltip]') //data-tooltip="bar-bar-bar"
+    const targetElement = document.querySelector('[data-tooltip]');
+
+    targetElement.addEventListener('pointerover', () => this.render());
+
+    //document.addEventListener('pointerover', () => this.render());
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+
+  destroy () {
+
+    this.element.remove();
+    Tooltip.linkToInstance.remove();
+  }
+
+
 }
+
 
 const tooltip = new Tooltip();
 
