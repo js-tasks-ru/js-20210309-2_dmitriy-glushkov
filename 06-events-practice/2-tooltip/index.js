55a51e34e708fea196b2ac3b3afe834e60d3e1f8
dglushko <dmitrii.glushkov@t-systems.com>
2021-04-01 14:18:50 +0300

    Tooltip ready to review

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -2,24 +2,15 @@ class Tooltip {
 
   static linkToInstance;
 
-  constructor() {
-
-
-  }
-
-
   render(event) {
 
     if (Tooltip.linkToInstance !== undefined) {
       Tooltip.linkToInstance.remove();
     }
 
-    const toolTipText = document.querySelector('[data-tooltip]').getAttribute('data-tooltip') //data-tooltip="bar-bar-bar"
-    //console.log('toolTipText', toolTipText)
-    //console.log('toolTipText', toolTipText.getAttribute('data-tooltip'))
+    const toolTipText = document.querySelector('[data-tooltip]').getAttribute('data-tooltip');
 
     const element = document.createElement('div');
-    //element.className = this.type;
     element.className = "tooltip";
     element.innerHTML = `
       <div>
@@ -29,56 +20,39 @@ class Tooltip {
     this.element = element;
 
     const targetElement = document.querySelector('[data-tooltip]');
-    //console.log('targetElement', targetElement)
-    //console.log('targetElement.getBoundingClientRect()', targetElement.getBoundingClientRect())
-
-    console.log('event.clientX', event.clientX)
-    console.log('event.clientY', event.clientY)
-
-    element.style.left = event.clientX + 5 + 'px';
+    element.style.left = event.clientX + 7 + 'px';
     element.style.top = event.clientY + 'px';
-
-    // element.style.left = targetElement.getBoundingClientRect().left + 5 + 'px';
-    // element.style.top = targetElement.getBoundingClientRect().bottom + 5 + 'px';
-
 
     document.body.append(this.element);
 
-
-
     Tooltip.linkToInstance = this;
+  }
 
-
+  changeTooltipText (event) {
+    this.element.innerHTML = `
+     <div>
+        ${event['target'].getAttribute('data-tooltip')}
+     </div>`;
   }
 
   initialize() {
-
-    //const toolTipText = document.querySelector('[data-tooltip]') //data-tooltip="bar-bar-bar"
     const targetElement = document.querySelector('[data-tooltip]');
 
-    //targetElement.addEventListener('pointerover', () => this.render('pointerover'));
+    targetElement.addEventListener('pointerover', (event) => this.render(event));
 
-    targetElement.addEventListener('pointerover', () => this.render(event));
-
+    targetElement.addEventListener('mouseover', (event) => this.changeTooltipText(event));
 
     targetElement.addEventListener('pointerout', () => this.destroy());
-    //document.addEventListener('pointerover', () => this.render());
-
-
   }
 
   remove() {
     this.element.remove();
   }
 
-
   destroy () {
-
     this.element.remove();
     Tooltip.linkToInstance.remove();
   }
-
-
 }
 
 
